baae8e997220586b3668e2886be3f51c4fe71e8e
managersingh <managersingh@outlook.com>
2020-12-04 20:15:52 +0530

    latest code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,14 +15,14 @@
                         <div class="container">
                             <div class="header-content">
                                 <div class="line"></div>
-                                <h2>Build your Next Holiday Trip with Us</h2>
-                                <h1><span>Create</span><span class="font-weight-bold"> Your Tour</span></h1>
-                                <h4><a href="{{ url('/contact') }}" class="slide-button text-white">Get in touch</a></h4>
+                                <h2>Understated Elegance with a <br>Contemporary Edge</h2>
+                                <!-- <h1><span>Create</span><span class="font-weight-bold"> Your Tour</span></h1>
+                                <h4><a class="slide-button">Get in touch</a></h4> -->
                             </div>
                         </div>
                      </div>
                 </div>                    
-                <div class="item">
+                <!-- <div class="item">
                     <img src="{{asset('images/backgrounds/2.jpg')}}" alt="images not found">
                     <div class="cover">
                         <div class="container">
@@ -30,37 +30,11 @@
                                 <div class="line animated bounceInLeft"></div>
                                 <h2>Enjoy the Best Destinations with Our Travel Agency</h2>
                                 <h1><span>Explore</span><span class="font-weight-bold"> The World</span></h1>
-                                <h4><a href="{{ url('/contact') }}" class="slide-button text-white">Get in touch</a></h4>
+                                <h4><a class="slide-button">Get in touch</a></h4>
                             </div>
                         </div>
                      </div>
-                </div>    
-				<div class="item">
-                    <img src="{{asset('images/exterior/3.jpg')}}" alt="images not found">
-                    <div class="cover">
-                        <div class="container">
-                            <div class="header-content">
-                                <div class="line"></div>
-                                <h2>Enjoy your refreshment in our fresh exterior</h2>
-                                <h1><span>Create</span><span class="font-weight-bold"> Your Tour</span></h1>
-                                <h4><a href="{{ url('/booking') }}" class="slide-button text-white">Book now</a></h4>
-                            </div>
-                        </div>
-                     </div>
-                </div>     
-				<div class="item">
-                    <img src="{{asset('images/interior/2.jpg')}}" alt="images not found">
-                    <div class="cover">
-                        <div class="container">
-                            <div class="header-content">
-                                <div class="line animated bounceInLeft"></div>
-                                <h2>Very gentle and friendly interior structure</h2>
-                                <h1><span>Explore</span><span class="font-weight-bold"> The World</span></h1>
-                                <h4><a href="{{ url('/booking') }}" class="slide-button text-white">Book now</a></h4>
-                            </div>
-                        </div>
-                     </div>
-                </div>           
+                </div>                 -->
                 
 				
             </div>       
@@ -116,106 +90,78 @@
 		</section>
 
 		<!-- Living Room 2 Section Start -->
-		<section class="full-row bg-white" style="z-index: 99;">
+		<section class="full-row bg-white interior" style="z-index: 99;">
 			<div class="container">
-				<div class="row">
+				<div class="row">				
 					<div class="col-lg-12">
 						<div class="main-title-area text-center">
 							<h2 class="title left-right-line mb-3">Interior</h2>
-						</div>
-						<!-- <span class="subtext text-secondary text-center mb-5 box-width">Across the street from the
-							ordinary</span> -->
-					</div>
-				</div>
-				<div class="row hover-shadow-lg py-4 wow fadeInRight" style="margin-bottom: 150px;animation-delay: 0.5s;">
-					<div class="col-lg-5">
-						<div class="img-slide text-width-medium text-spacing-75 wow fadeInLeft" style="animation-delay: 0.3s;">
-							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-								<div class="carousel-inner">
-									<div class="carousel-item active"> <img src="{{ asset('images/interior/1.jpg') }}" alt="Image not found!">
-									</div>
-									<div class="carousel-item"> <img src="{{ asset('images/interior/2.jpg') }}" alt="Image not found!"> </div>
-									<div class="carousel-item"> <img src="{{ asset('images/interior/3.jpg') }}" alt="Image not found!"> </div>
-								</div>
-								<div class="slider-arrow"> 
-									<a class="carousel-control-prev" href="#carouselExampleIndicators"
-										role="button" data-slide="prev"> <i class="fas fa-angle-double-left"></i> </a> 
-									<a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-										data-slide="next"> <i class="fas fa-angle-double-right"></i> </a> 
-								</div>
-							</div>
-						</div>
-						<div class="room-detail-info bg-gray">
-							<div class="float-left">
-								<h3>Interior</h3>
-							</div>
-							<div class="float-right items-end">
-								<a href="{{ url('/vessel') }}" class="btn btn-primary-bg" role="button">
-									View More
-								</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-7">
-						<div class="text-right py-4 px-4">
-							<p>
-							<h6 class="" style="animation-delay: 0.2s;">MY Giorgia can accommodate up to ten guests in five well-appointed staterooms, all featuring ensuite marble bathrooms. </h6>
-
+							<p><h6 class="" style="animation-delay: 0.2s;">MY Giorgia can accommodate up to ten guests in five well-appointed staterooms, all featuring ensuite marble bathrooms. </h6>
 							<p  class="" style="animation-delay: 0.4s;">The master suite, found on the main deck, comes with a walk-through study and double sinks. Below deck you will find two VIP suites and twin Guest suites. 
-
-							Classically designed interiors and minimalist dark wood bulkheads contribute to a contemporary ambience, while plush furnishings create a sense of warmth, elegance and comfort.</p>
-							</p>
+							Classically designed interiors and minimalist dark wood bulkheads contribute to a contemporary ambience, while plush furnishings create a sense of warmth, elegance and comfort.</p></p>
+						</div>
+			
+						
+						<div class="row">
+						<div class="col-lg-4">
+							<div class="image-inter">
+							<img src="{{ asset('images/interior/1.jpg') }}" alt="Image not found!">
+							</div>
+						</div>
+						<div class="col-lg-4">
+						<div class="image-inter">
+							<img src="{{ asset('images/interior/2.jpg') }}" alt="Image not found!">
+							</div>
+						</div>
+						<div class="col-lg-4">
+						<div class="image-inter">
+							<img src="{{ asset('images/interior/3.jpg') }}" alt="Image not found!">
+							</div>
 						</div>
 					</div>
-				</div>
+					<div class="text-center items-end mt-4">
+						<a href="{{ url('/vessel') }}" class="btn btn-primary-bg" role="button">
+							View More
+						</a>
+					</div>					
+				</div>				
+			</div>
+		</section>
+
+		<!-- Living Room 2 Section Start -->
+		<section class="full-row bg-grey bg-colorone exterior" style="z-index: 99;">
+			<div class="container">				
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="main-title-area text-center">
 							<h2 class="title left-right-line mb-3">Exterior</h2>
-						</div>
-						<!-- <span class="subtext text-secondary text-center mb-5 box-width">Across the street from the
-							ordinary</span> -->
-					</div>
-				</div>
-				<div class="row hover-shadow-lg py-4 wow fadeInLeft" style="animation-delay: 0.6s;">            
-					<div class="col-lg-7">
-						<div class="py-4 px-4">
-							<p>
-							<h6 class="" style="animation-delay: 0.2s;" > Giorgia is a true masterpiece of high-end yachting design, featuring teak beams and elegant, no-fuss exteriors. Wherever you are going you can rest assured that you will arrive in style.</h6>
-
+							<p><h6 class="" style="animation-delay: 0.2s;" > Giorgia is a true masterpiece of high-end yachting design, featuring teak beams and elegant, no-fuss exteriors. Wherever you are going you can rest assured that you will arrive in style.</h6>
 							<p class="" style="animation-delay: 0.4s;">A plethora of high-end entertainment and leisure facilities make Giorgia perfect for enjoying a family vacation, socializing with friends, or even more formal endeavours.  
-
-							With a choice of three decks, all featuring their own dining space, you can savour your meals al fresco, sip cocktails under the starry sky at the sundeck bar, or indulge in strawberries and champagne while soaking in the spa pool.
-</p></p>
+							With a choice of three decks, all featuring their own dining space, you can savour your meals al fresco, sip cocktails under the starry sky at the sundeck bar, or indulge in strawberries and champagne while soaking in the spa pool.</p></p>
 						</div>
-					</div>
-					<div class="col-lg-5">
-						<div class="img-slide img-slide text-width-medium text-spacing-75 wow fadeInRight animated" style="animation-delay: 0.3s;">
-							<div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel">
-								<div class="carousel-inner">
-									<div class="carousel-item active"> <img src="{{ asset('images/exterior/1.jpg') }}" alt="Image not found!">
-									</div>
-									<div class="carousel-item"> <img src="{{ asset('images/exterior/2.jpg') }}" alt="Image not found!"> </div>
-									<div class="carousel-item"> <img src="{{ asset('images/exterior/3.jpg') }}" alt="Image not found!"> </div>
-								</div>
-								<div class="slider-arrow"> <a class="carousel-control-prev" href="#carouselExampleIndicators1"
-										role="button" data-slide="prev"> <i class="fas fa-angle-double-left"></i> </a> <a
-										class="carousel-control-next" href="#carouselExampleIndicators1" role="button"
-										data-slide="next"> <i class="fas fa-angle-double-right"></i> </a> 
-								</div>
+						<div class="row">
+						<div class="col-lg-4">
+							<div class="image-inter">
+							<img src="{{ asset('images/exterior/01.jpg') }}" alt="Image not found!">
 							</div>
 						</div>
-						<div class="room-detail-info bg-gray">
-							<div class="float-right">
-								<h3>Exterior</h3>
+						<div class="col-lg-4">
+						<div class="image-inter">
+						<img src="{{ asset('images/exterior/2.jpg') }}" alt="Image not found!">
 							</div>
-							<div class="float-left items-end">
-								<a href="{{ url('/vessel') }}" class="btn btn-primary-bg" role="button">
+						</div>
+						<div class="col-lg-4">
+						<div class="image-inter">
+						<img src="{{ asset('images/exterior/3.jpg') }}" alt="Image not found!">
+							</div>
+						</div>
+						</div>
+						<div class="text-center items-end mt-4">
+							<a href="{{ url('/vessel') }}" class="btn btn-primary-bg" role="button">	
 									View More
-								</a>
-							</div>
-						</div>
-					</div>
+							</a>
+						</div>						
+									
 				</div>
 			</div>
 		</section>
@@ -235,11 +181,14 @@
 								<h2 class="title left-right-line mb-3">Specifications</h2>
 							</div>
 							<!-- <span class="subtext text-secondary text-center mb-5 box-width">Across the street from the ordinary</span> </div> -->
+						</div>
 					</div>
-					<div id="slider5" class="owl-carousel slider-5" style="z-index: 1;">
-				<!-- Slide 1-->
-				<div class="item active" data-ls="bgsize:cover; bgposition:50% 50%; duration:8000; transition2d:3; timeshift:-500; deeplink:home; kenburnszoom:in; kenburnsrotate:0; kenburnsscale:1.1; parallaxevent:scroll; parallaxdurationmove:500;">
-					<table class="specification">
+				</div>
+
+				<div class="owl-slider">
+	<div id="carousel" class="owl-carousel">
+	<div class="item">
+	<table class="specification">
 						<tbody><tr class="fadeInLeftBig">
 							<th>Length</th>
 							<td>36.6 m (120’)</td>
@@ -266,17 +215,13 @@
 						</tr>
 						<tr>
 							<th>Flag</th>
-							<td><img class="float-right" src="http://127.0.0.1:8000/images/flag/manflag.png" style="width: 50px; height: 30px"></td>
+							<td><img src="http://127.0.0.1:8000/images/flag/manflag.png" style="width: 50px; height: 30px"></td>
 						</tr>
 						
 					</tbody></table>
-					
-					
-				</div>
-
-				<!-- Slide 2-->
-				<div class="item" data-ls="bgsize:cover; bgposition:50% 50%; duration:8000; transition2d:3; deeplink:latest-works; kenburnszoom:out; kenburnsrotate:0; kenburnsscale:1.1; parallaxtype:3d; parallaxevent:scroll; parallaxdurationmove:500; parallaxrotate:4;">
-					<table class="specification">
+	</div>
+	<div class="item">
+	<table class="specification">
 						<tbody>
 						<tr>
 							<th>Hull</th>
@@ -306,14 +251,17 @@
 							<th>Range</th>
 							<td>3,500 mm</td>
 						</tr>
-					</tbody></table>					
-				</div>
+					</tbody></table>
+	</div>
+	
+</div>
+</div>
 
-				
-			</div>
-					
-				</div>
-			</div>
+
+
+
+			</div>					
+		</div>
 		</section>
 
 		<section class="full-row bg-white">
@@ -430,28 +378,37 @@
 })()
 </script>
 <script type="text/javascript">
-	$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:false,
-    nav:true,
-    mouseDrag:false,
-    autoplay:true,
-    animateOut: 'slideOutLeft',
-    responsive:{
-        0:{
-            items:1
-        },
-        // 600:{
-        //     items:1
-        // },
-        // 1200:{
-        //     items:1
-        // },
-		// 1800:{
-        //     items:1
-        // }
+jQuery("#carousel").owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  loop: true,
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 10000,
+  smartSpeed: 1800,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1
+    },
+
+    1024: {
+      items: 1
+    },
+
+    1366: {
+      items: 1
     }
+  }
 });
 </script>
 
